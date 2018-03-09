8df8cbc99c450ddfd3b8dcb9b4d9bf2fc1d1fadd
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 17:27:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
-function forLoop(Array){
-  for (let i=0; i<25; i++){
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
     if (i === 1) {
       array.push("I am 1 strange loop.");
     }
@@ -7,6 +7,5 @@ function forLoop(Array){
       array.push("I am ${i} strange loops.");
     }
   }
+  return array;
 }
- return Array;
-
